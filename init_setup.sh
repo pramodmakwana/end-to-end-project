d3d50aa6aa0def73e3ef864ad9d51f498164e086
pramod makwana <pramod1117789@gmail.com>
2023-10-26 23:35:37 +0530

    i created a shell script

--- init_setup.sh
+++ init_setup.sh
@@ -1,23 +1,3 @@
-# This is my end to end project
-
-# First initialize the git
-
-git init
-
-
-
-git add{file name.txt}
-
-# to add folder in git
-git add.
-
-git commit -m "This is my first commit"
-
-# for faching any changes in git hub enviorment below command
-git pull
-
-
-
 echo [$(date)]: "START"
 
 echo [$[date]]: "creating env with python 3.8 version"
